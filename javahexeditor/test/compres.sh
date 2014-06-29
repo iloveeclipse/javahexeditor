fc162a1513445b8f171978d6f8b858bf7cc1ab28
Peter Dell <Peter Dell@1b7e8f17-9e7c-4ed6-b75c-8dcd1dfa6bb3>
2014-06-29 22:27:01 +0000

    Refactoring, plugin part

--- javahexeditor/test/compres.sh
+++
@@ -1,4 +0,0 @@
-export TESTFILES=./net/sourceforge/javahexeditor
-gcj -c -g0 -O2 -v --resource TestsData.hex $TESTFILES/TestsData.hex -o ../../bin/testsdatahex.o
-gcj -c -g0 -O2 -v --resource TestsLongData.hex $TESTFILES/TestsLongData.hex -o ../../bin/testslongdatahex.o
-gcj -c -g0 -O2 -v --resource TestsUnicode.hex $TESTFILES/TestsUnicode.hex -o ../../bin/testsunicodehex.o
