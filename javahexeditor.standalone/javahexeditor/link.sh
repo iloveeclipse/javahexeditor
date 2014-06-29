0b3269bf8f13f571aad39b7d5828aeb782099e50
Peter Dell <Peter Dell@1b7e8f17-9e7c-4ed6-b75c-8dcd1dfa6bb3>
2014-06-29 22:39:01 +0000

    Cleanup

--- javahexeditor.standalone/javahexeditor/link.sh
+++
@@ -1,3 +0,0 @@
-gcj -v -s -Wl,--subsystem=windows --main=net.sourceforge.javahexeditor.Manager \
--static-libgcj -static-libgcc -o ../bin/javahexeditor-win32_.exe ../bin/hexpng.o -L../bin -ljavahexeditor \
--lswt4javahexeditor ../bin/userguidehtml.o ../bin/swt-so.o
