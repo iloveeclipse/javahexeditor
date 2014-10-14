eec02464634bf3dd7ef5bebb5c42c58659203546
Peter Dell <Peter Dell@1b7e8f17-9e7c-4ed6-b75c-8dcd1dfa6bb3>
2014-10-14 19:56:24 +0000

    Cleanup and localization, new icons (ICO/PNG), first part of Germand localization (plugin only)

--- trunk/javahexeditor/bak/buildtest.sh
+++
@@ -1,8 +0,0 @@
-export SWT_JAR_LIB_NAME="swt-3.5.1-win32-win32-x86"
-export PATH_SEPARATOR=";"
-
-gcj -g0 -O2 -s -v -fsource=1.4 -static-libgcj -static-libgcc \
---classpath=../src${PATH_SEPARATOR}../../swt/$SWT_JAR_LIB_NAME/swt.jar${PATH_SEPARATOR}junit3.8.2/junit.jar \
--o ../../bin/test.exe --main=net.sourceforge.javahexeditor.AllTests -L../../bin `find ./net -name "*.java"` \
--ljavahexeditor -ljunit -lswt4javahexeditor ../../bin/swt-so.o ../../bin/testsdatahex.o \
-../../bin/testslongdatahex.o ../../bin/testsunicodehex.o
